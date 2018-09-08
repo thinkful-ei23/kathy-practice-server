b68ccd3c15de7bf82c146050575e788ec51a5d44
kathyleboeuf <kleboeuf@townisp.com>
2018-09-08 12:43:10 -0400

    added API endpoint get/cheeses

--- index.js
+++ index.js
@@ -21,6 +21,27 @@ app.use(
     origin: CLIENT_ORIGIN
   })
 );
+app.get('/api/cheeses', (req, res) => {
+  return res.json([
+    "Bath Blue",
+    "Barkham Blue",
+    "Buxton Blue",
+    "Cheshire Blue",
+    "Devon Blue",
+    "Dorset Blue Vinney",
+    "Dovedale",
+    "Exmoor Blue",
+    "Harbourne Blue",
+    "Lanark Blue",
+    "Lymeswold",
+    "Oxford Blue",
+    "Shropshire Blue",
+    "Stichelton",
+    "Stilton",
+    "Blue Wensleydale",
+    "Yorkshire Blue"
+  ]);
+});
 
 function runServer(port = PORT) {
   const server = app
