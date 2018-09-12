dc737a99c4aae33330f180f42f977a32c131d3d4
kathyleboeuf <kleboeuf@townisp.com>
2018-09-11 22:53:56 -0400

    working on endpoint

--- index.js
+++ index.js
@@ -3,45 +3,76 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-
+const knex = require('knex');
+const bodyParser = require('body-parser');
+const { dbConnect } = require('./db-knex');
 const { PORT, CLIENT_ORIGIN } = require('./config');
-const { dbConnect } = require('./db-mongoose');
-// const {dbConnect} = require('./db-knex');
+//const { dbConnect } = require('./db-mongoose');
 
 const app = express();
+const jsonParser = bodyParser.json();
 
+app.use(express.json());
+//app.use(bodyParser.json());
+app.use(express.static('public'));
 app.use(
   morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
     skip: (req, res) => process.env.NODE_ENV === 'test'
   })
 );
-
-app.use(
-  cors({
-    origin: CLIENT_ORIGIN
-  })
+app.use(cors({ origin: CLIENT_ORIGIN })
 );
-app.get('/api/cheeses', (req, res) => {
-  // return
-  res.json([
-    "Bath Blue",
-    "Barkham Blue",
-    "Buxton Blue",
-    "Cheshire Blue",
-    "Devon Blue",
-    "Dorset Blue Vinney",
-    "Dovedale",
-    "Exmoor Blue",
-    "Harbourne Blue",
-    "Lanark Blue",
-    "Lymeswold",
-    "Oxford Blue",
-    "Shropshire Blue",
-    "Stichelton",
-    "Stilton",
-    "Blue Wensleydale",
-    "Yorkshire Blue"
-  ]);
+
+// app.use("/api/teacher", teacherRouter);
+// app.use("/api/student", studentRouter);
+// app.use("/api/course", courseRouter);
+
+//===========GET TEACHER==============
+app.get('/api/teachers', jsonParser, (req, res, next) => {
+  //let filter = {};
+  teachers.filter()
+    .then(results => {
+      res.json(results)
+    })
+    .catch(err => {
+      next(err)
+    })
+})
+//==========POST TEACHER=================
+app.post('/api/teachers', jsonParser, (req, res, next) => {
+
+  const { id, first_name, last_name, email, password, teacher_code } = req.body;
+  console.log(req.body)
+
+  /***** Never trust users. Validate input *****/
+  // if (!first_name) {
+  //   const err = new Error('Missing `first_name` in request body');
+  //   err.status = 400;
+  //   return next(err);
+  // }
+  const newTeacher = {
+    id: id,
+    first_name: first_name,
+    last_name: last_name,
+    email: email,
+    password: password,
+    teacher_code: teacher_code
+  };
+
+  knex('teachers')
+    .insert(newTeacher)
+    .into('teachers')
+    .returning(['id', 'first_name', 'last_name', 'email', 'password', 'teacher_code'])
+    .then((results) => {
+      const result = results[0];
+      res
+        .location(`${req.originalUrl}/${result.id}`)
+        .status(201)
+        .json(result);
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 function runServer(port = PORT) {
--- server.js
+++ server.js
@@ -1,20 +1,66 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+//const express = require('express');
+//const { PORT } = require('./config');
+//const dbConnect = require('./db-knex')
 
-const app = express();
+//const app = express();
+// app.use(express.json());
+// app.use(express.static('public'));
+//app.use(
+// 	cors({
+// 		origin: CLIENT_ORIGIN
+// 	})
+// );
+//========== POST NEW TEACHER ===============
 
-app.use(
-	cors({
-		origin: CLIENT_ORIGIN
+
+
+
+
+app.listen(process.env.PORT, () => console.log(
+	`Your app is listening on port ${process.env.PORT}`));
+
+app.get('/api/cheeses', (req, res) => {
+	// return
+	res.json([
+		"Bath Blue",
+		"Barkham Blue",
+		"Buxton Blue",
+		"Cheshire Blue",
+		"Devon Blue",
+		"Dorset Blue Vinney",
+		"Dovedale",
+		"Exmoor Blue",
+		"Harbourne Blue",
+		"Lanark Blue",
+		"Lymeswold",
+		"Oxford Blue",
+		"Shropshire Blue",
+		"Stichelton",
+		"Stilton",
+		"Blue Wensleydale",
+		"Yorkshire Blue"
+	]);
+});
+
+/*
+knex
+	.select('teacher.id', 'first_name', 'last_name')
+	.from('teacher')
+	.modify(queryBuilder => {
+		if (searchTerm) {
+			queryBuilder.where('title', 'like', `%${searchTerm}%`);
+		}
 	})
-);
+	.orderBy('teacher.id')
+	.then(results => {
+		console.log(JSON.stringify(results, null, 2));
+	})
+	.catch(err => {
+		console.error(err);
+	});
+*/
 
-
-app.post('/api/login', bodyParser.json(), (req, res)) => {
-
-	//validate field types
-	if (req.body.)
-
-
-
-}
+	// app.get('/api/teacher/:id', (req, res) => {
+// 	const id = req.params.id;
+// 	res.json(data.find(item => item.id === Number(id)))
+// };
