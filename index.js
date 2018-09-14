eb031e5d69f0da41a5e9c658542271fa8e8020f0
kathyleboeuf <kleboeuf@townisp.com>
2018-09-14 13:51:49 -0400

    starting to work on endpoints

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 const jsonParser = bodyParser.json();
 
 app.use(express.json());
-//app.use(bodyParser.json());
+app.use(bodyParser.json());
 app.use(express.static('public'));
 app.use(
   morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
@@ -23,21 +23,107 @@ app.use(
 );
 app.use(cors({ origin: CLIENT_ORIGIN })
 );
-//============SIGN IN ===================
+//======================
+//ENDPOINTS
+//============SIGN UP STUDENT===================
+app.post('/api/auth/signinS', jsonParser, (req, es, next) => {
+  const { id, name, last_name, email, password, teacher_id } = req.body;
+  //console.log(req.body)
+
+  /***** Never trust users. Validate input *****/
+  // if (!first_name) {
+  //   const err = new Error('Missing `first_name` in request body');
+  //   err.status = 400;
+  //   return next(err);
+  // }
+  const newStudent = {
+    id: id,
+    name: name,
+    last_name: last_name,
+    email: email,
+    password: password,
+    teacher_id: teacher_id
+  };
+
+  knex('students')
+    .insert(newStudent)
+    .into('students')
+    .returning(['id', 'name', 'last_name', 'email', 'password', 'teacher_id'])
+    .then((results) => {
+      const result = results[0];
+      res
+        .location(`${req.originalUrl}/${result.id}`)
+        .status(201)
+        .json(result);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+
+//============SIGN UP TEACHER===================
+app.post('/api/auth/signinT', jsonParser, (req, es, next) => {
+  const { id, name, last_name, email, password } = req.body;
+  //console.log(req.body)
+
+  /***** Never trust users. Validate input *****/
+  // if (!first_name) {
+  //   const err = new Error('Missing `first_name` in request body');
+  //   err.status = 400;
+  //   return next(err);
+  // }
+  const newTeacher = {
+    first_name: first_name,
+    last_name: last_name,
+    email: email,
+    password: password,
+    teacher_code: 9876
+  };
+
+
+  knex('teachers')
+    .insert(newTeacher)
+    .into('teachers')
+    .returning(['id', 'name', 'last_name', 'email', 'password', 'teacher_id'])
+    .then((results) => {
+      const result = results[0];
+      res
+        .location(`${req.originalUrl}/${result.id}`)
+        .status(201)
+        .json(result);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
 //============LOG IN ===================
 //POST req
 //app.login('./api/auth/login', )
 //check if already exists
+app.post('/api/auth/login', jsonParser, (req, es, next) => {
+  knex()
+})
 
-
-//return fetch(`${API_BASE_URL}/protected`,
-
-//===========GET TEACHER==============WORKS
+//===========GET ALL TEACHERS==============WORKS
 app.get('/api/teachers', jsonParser, (req, res, next) => {
   //console.log(knex.raw, 'knex raw not cooked')
   knex('teachers')
     .select('id', 'first_name', 'last_name', 'password', 'email')
     // .from('teachers')
+    .then(results => {
+      res.json(results)
+    })
+    .catch(err => {
+      next(err)
+    })
+})
+
+//===========GET TEACHER by ID==============
+app.get('/api/teachers/:id', jsonParser, (req, res, next) => {
+  knex('teachers')
+    .select('id', 'first_name', 'last_name', 'password', 'email')
+    .from('teachers')
     .then(results => {
       res.json(results)
     })
@@ -81,9 +167,54 @@ app.post('/api/teachers', jsonParser, (req, res, next) => {
       next(err);
     });
 });
-//========UPDATE TEACHER ===========
+//========PUT / UPDATE TEACHER ===========
+app.put('/api/teachers/:id', jsonParser, (req, res, next) => {
+  const teacher_id = req.params.id;
+  const { first_name, last_name, email, password } = req.body;
+
+  // /***** Never trust users. Validate input *****/
+  // if (!teacher_id) {
+  //   const err = new Error('Missing `teacher_id` in request body');
+  //   err.status = 400;
+  //   return next(err);
+  // }
+
+  const updateTeacher = {
+    first_name: first_name,
+    last_name: last_name,
+    email: email,
+    password: password,
+  };
+
+  knex.insert('teachers')
+    .update(updateTeacher)
+    .where('id', teacher_id)
+    .returning(['id', 'first_name', 'last_name', 'email', 'password '])
+    .then(([result]) => {
+      if (result) {
+        res.json(result);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
 
 //=========DELETE TEACHER ==========
+app.delete('/:id', (req, res, next) => {
+  knex.del()
+    .where('id', req.params.id)
+    .from('notes')
+    .then(() => {
+      res.status(204).end();
+    })
+    .catch(err => {
+      next(err);
+    });
+});
 
 //========GET STUDENT ==============WORKS
 app.get('/api/students', jsonParser, (req, res, next) => {
@@ -149,7 +280,7 @@ app.get('/api/courses', jsonParser, (req, res, next) => {
       next(err)
     })
 })
-//=========POST CARD-COURSE ============
+//=========POST CARD-COURSE ============WORKS
 app.post('/api/courses', jsonParser, (req, res, next) => {
 
   const { id, title, description, teacher_id } = req.body;
